38b386e61298de4afd46347de39fbb13955dc3aa
Rupali Rajput <rupali.rajput@wsu.edu>
2019-05-12 23:58:23 -0700

    wip

---
+++ App.js
@@ -0,0 +1,157 @@
+"use strict";
+exports.__esModule = true;
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+//var MongoClient = require('mongodb').MongoClient;
+//var Q = require('q');
+var AccountModel_1 = require("./model/AccountModel");
+var QuestionBankModel_1 = require("./model/QuestionBankModel");
+var QuestionsModel_1 = require("./model/QuestionsModel");
+var ReportModel_1 = require("./model/ReportModel");
+// Creates and configures an ExpressJS web server.
+var App = /** @class */ (function () {
+    //Run configuration methods on the Express instance.
+    function App() {
+        this.expressApp = express();
+        this.middleware();
+        this.routes();
+        this.idGenerator = 200;
+        this.Accounts = new AccountModel_1.AccountModel();
+        this.Reports = new ReportModel_1.ReportModel();
+        //this.Tasks = new TaskModel();
+        this.QuestionBanks = new QuestionBankModel_1.QuestionBankModel();
+        this.Questions = new QuestionsModel_1.QuestionsModel();
+    }
+    // Configure Express middleware.
+    App.prototype.middleware = function () {
+        this.expressApp.use(logger('dev'));
+        this.expressApp.use(bodyParser.json());
+        this.expressApp.use(bodyParser.urlencoded({ extended: false }));
+    };
+    // Configure API endpoints.
+    App.prototype.routes = function () {
+        var _this = this;
+        var router = express.Router();
+        router.get('/app/account/', function (req, res) {
+            console.log('Query All account');
+            _this.Accounts.retrieveAllAcccounts(res);
+        });
+        // get API for retriving single account by userid
+        router.get('/app/account/:userid', function (req, res) {
+            var id = req.params.userid;
+            console.log('Query single user with id: ' + id);
+            _this.Accounts.retrieveAccountDetails(res, { userid: id });
+        });
+        // post API for creating an account
+        router.post('/app/account/', function (req, res) {
+            console.log(req.body);
+            var jsonObj = req.body;
+            jsonObj.userid = _this.idGenerator;
+            _this.Accounts.model.create([jsonObj], function (err) {
+                if (err) {
+                    console.log('account creation failed');
+                }
+            });
+            res.send(_this.idGenerator.toString());
+            _this.idGenerator++;
+        });
+        // get API for getting all reports
+        router.get('/app/report/:userid/reports', function (req, res) {
+            var id = req.params.userid;
+            console.log("Query single user's reports with id:" + id);
+            _this.Reports.retrieveAllReportDetails(res, { userid: id });
+        });
+        // get API for getting a single report
+        router.get('/app/report/:userid/reports/:reportid', function (req, res) {
+            var id = req.params.userid;
+            var reportid = req.params.reportid;
+            console.log("Query a single report from a single user with user id:" + id + " and report id: " + reportid);
+            _this.Reports.retrieveSingleReportDetails(res, { userid: id, reportid: reportid });
+            // retrive all questionBanks
+            router.get('/app/questionBanks/', function (req, res) {
+                console.log('Query All questionBanks');
+                _this.QuestionBanks.retrieveAllQuestionBanks(res);
+            });
+            // retrive questionBank with ID
+            router.get('/app/questionBanks/:quesBankID/', function (req, res) {
+                var id = req.params.quesBankID;
+                console.log('Query single list with id: ' + id);
+                _this.QuestionBanks.retrieveQuestionBankDetails(res, { quesBankID: id });
+            });
+            // post data in questionBank
+            router.post('/app/questionBanks/', function (req, res) {
+                console.log(req.body);
+                var jsonObj = req.body;
+                jsonObj.quesBankID = _this.idGenerator;
+                _this.QuestionBanks.model.create([jsonObj], function (err) {
+                    if (err) {
+                        console.log('object creation failed');
+                    }
+                });
+                res.send(_this.idGenerator.toString());
+                _this.idGenerator++;
+            });
+            // delete question bank
+            router["delete"]('/app/questionBanks/:quesBankID/', function (req, res) {
+                var id = req.params.quesBankID;
+                console.log('Delete QuestionBank with id: ' + id);
+                _this.QuestionBanks.deleteQuestionBank(res, { quesBankID: id });
+            });
+            // update question bank
+            router.put('/app/questionBanks/:quesBankID/', function (req, res) {
+                console.log(req.body);
+                var jsonObj = req.body;
+                var id = req.params.quesBankID;
+                jsonObj.quesBankID = id;
+                _this.QuestionBanks.model.update([jsonObj], { questionid: id }, function (err) {
+                    if (err) {
+                        console.log('object creation failed');
+                    }
+                });
+                res.send(_this.idGenerator.toString());
+            });
+            // get all questions 
+            router.get('/app/questions/', function (req, res) {
+                console.log('Query All questions');
+                _this.Questions.retrieveAllQuestions(res);
+            });
+            // get questions of a particular question bank
+            router.get('/app/questions/:quesBankID', function (req, res) {
+                var id = req.params.quesBankID;
+                console.log('Query single list with id: ' + id);
+                _this.Questions.retrieveQuestionsDetails(res, { quesBankID: id });
+            });
+            // post data into questions table
+            router.post('/app/questions/', function (req, res) {
+                console.log(req.body);
+                var jsonObj = req.body;
+                jsonObj.quesBankID = _this.idGenerator;
+                _this.Questions.model.create([jsonObj], function (err) {
+                    if (err) {
+                        console.log('object creation failed');
+                    }
+                });
+                res.send(_this.idGenerator.toString());
+                _this.idGenerator++;
+            });
+            // delete question bank
+            router["delete"]('/app/questions/:quesid/', function (req, res) {
+                var id = req.params.quesid;
+                console.log('Delete QuestionBank with id: ' + id);
+                _this.Questions.deleteQuestion(res, { quesid: id });
+            });
+        });
+        this.expressApp.use('/', router);
+        this.expressApp.use('/app/json/', express.static(__dirname + '/app/json'));
+        this.expressApp.use('/images', express.static(path.join(__dirname, '/images')));
+        this.expressApp.use('/css', express.static(path.join(__dirname, '/css')));
+        this.expressApp.use('/fonts', express.static(path.join(__dirname, '/fonts')));
+        this.expressApp.use('/img', express.static(path.join(__dirname, '/img')));
+        this.expressApp.use('/js', express.static(path.join(__dirname, '/js')));
+        this.expressApp.use('/', express.static(path.join(__dirname, '/pages')));
+    };
+    return App;
+}());
+exports.App = App;
---
+++ createDB/createQuizAppSampleData.js
@@ -0,0 +1,129 @@
+﻿db = db.getSiblingDB('QuizAppSample')
+db.createCollection('accounts')
+accountsCollection = db.getCollection("accounts")
+accountsCollection.remove({})
+accountsCollection.insert(
+{
+	  username: "ABC",
+      password: "1234",
+      userid: 1,
+	  firstName: "ABC",
+      lastName: "XYZ",
+      email: "abc@gmail.com",
+      role: "student"
+
+}
+)
+accountsCollection.insert(
+{
+      username: "PQR",
+      password: "1234",
+      userid: 2,
+      firstName: "PQR",
+      lastName: "OPQ",
+      email: "pqr@gmail.com",
+      role: "professor"
+}
+)
+db.createCollection('reports')
+reportCollection = db.getCollection("reports")
+reportCollection.remove({})
+reportCollection.insert(
+{
+      reportid: 1,
+      userid: 1,
+      quesBankID: 101,
+      score: 87,
+      strengths: "Slopes of lines",
+      weaknesses: "Horizontal and Vertical Lines"
+    }
+)
+reportCollection.insert(
+{
+    reportid:2,
+    userid: 1,
+    quesBankID: 102,
+    score: 65,
+    strengths: "Newton's Laws of Motion",
+    weaknesses: "Fluid Dynamics"
+    
+    }
+    )
+db.createCollection('questionBanks')
+questionBanksCollection = db.getCollection("questionBanks")
+questionBanksCollection.remove({})
+questionBanksCollection.insert(
+      {
+            quesBankID: 101,
+            quesBankName: "Mathematics",
+            status: "Published",
+            createdDate: "05/07/2019",
+            lastmodifiedDate: "05/08/2019",
+            createdBy: "Prof. A",
+            updatedBy: "Prof. H"
+
+      }
+)
+questionBanksCollection.insert(
+      {
+            quesBankID: 102,
+            quesBankName: "Physics",
+            status: "Not Published",
+            createdDate: "04/07/2019",
+            lastmodifiedDate: "04/08/2019",
+            createdBy: "Prof. H",
+            updatedBy: "Prof. H"
+
+      }
+)
+db.createCollection('questions')
+questionsCollection = db.getCollection("questions")
+questionsCollection.remove({})
+questionsCollection.insert(
+      {
+            quesBankID: 101,
+            questions: [
+                  {
+                        quesid : 1011,
+                        questiontext : "What is the square of 12?",
+                        description : "Mathematics",
+                        options:[{opt1:"140",opt2:"143",opt3:"12",opt4:"144"}],
+                        answer: "144"
+
+                  },
+                  {
+                        quesid : 1012,
+                        questiontext : "What is the squareroot of 169?",
+                        description : "Mathematics",
+                        options:[{opt1:"12",opt2:"13",opt3:"11",opt4:"14"}],
+                        answer: "13"
+
+                  }
+            ]
+
+      }
+)
+questionsCollection.insert(
+      {
+            quesBankID: 102,
+            questions: [
+                  {
+                        quesid : 1021,
+                        questiontext : "Which of below has both magnitude and direction?",
+                        description : "Physics",
+                        options:[{opt1:"scaler",opt2:"vector"}],
+                        answer: "vector"
+
+                  },
+                  {
+                        quesid : 1022,
+                        questiontext : "What is the formula to calculate speed?",
+                        description : "Physics",
+                        options:[{opt1:"speed = distance/time",opt2:"speed = distance * time",opt3:"speed = time/distance",opt4:"None of the above"}],
+                        answer: "speed = distance/time"
+
+                  }
+            ]
+
+      }
+)
