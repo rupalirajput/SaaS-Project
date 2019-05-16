eaf065cf5ed7e24609858d7a534c8c0173a9a436
Chris Choi <choic2@seattleu.edu>
2019-05-16 16:25:43 -0700

    changed report model back to how it was

--- model/ReportModel.ts
+++ model/ReportModel.ts
@@ -23,7 +23,7 @@ class ReportModel {
                 score: Number,
                 strengths: String,
                 weaknesses: String
-            }, {collection: 'reports'}
+            },
         );
     }
 
