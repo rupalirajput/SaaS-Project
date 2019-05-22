fad573a7cb9d750542b13941d43a40736d67388e
Chris Choi <choic2@seattleu.edu>
2019-05-22 16:04:33 -0700

    saving some changes

--- DataAccess.ts
+++ DataAccess.ts
@@ -3,7 +3,7 @@ import Mongoose = require("mongoose");
 class DataAccess {
     static mongooseInstance: any;
     static mongooseConnection: Mongoose.Connection;
-    static DB_CONNECTION_STRING:string = 'mongodb://dbAdmin:test@localhost:27017/QuizAppSample?authSource=admin';
+    static DB_CONNECTION_STRING:string = 'mongodb://dbAdmin:test@localhost:3000/QuizAppSample?authSource=admin';
  
     constructor () {
         DataAccess.connect();
